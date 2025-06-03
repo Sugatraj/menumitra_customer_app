82cacd8275861e479c93b65e326e1f719ec4afac
Sugatraj <sugatraj.2106@gmail.com>
2025-06-03 11:20:54 +0530

    order details changes

--- src/layouts/OrderDetail.jsx
+++ src/layouts/OrderDetail.jsx
@@ -15,52 +15,48 @@ function OrderDetail() {
             <h5 className="title border-bottom pb-1 font-w600">
               Order #{orderId}
             </h5>
-            <span className="status-badge completed">
-              Completed
+            <span className="status-badge">
+              Cooking
             </span>
           </div>
           <div className="order-summery">
             <ul className="summery-list mb-4">
               <li>
-                <p className="order-name">Lemon Rice</p>
+                <p className="order-name">Testing Menu</p>
                 <div className="d-flex align-items-center">
-                  <span className="badge bg-light text-dark me-2">Veg</span>
-                  <span className="order-quantity">x1</span>
+                  <span className="badge bg-light text-dark me-2">Nonveg</span>
+                  <span className="order-quantity">x2</span>
                 </div>
               </li>
               <li>
                 <h6 className="mb-0 font-12">Bill Amount</h6>
-                <span className="font-12 font-w600 text-dark">₹180.00</span>
+                <span className="font-12 font-w600 text-dark">₹150.00</span>
               </li>
               <li>
                 <h6 className="mb-0 font-12">Service Charge (1%)</h6>
-                <span className="font-12 font-w600 text-dark">₹1.74</span>
+                <span className="font-12 font-w600 text-dark">₹1.47</span>
               </li>
               <li>
                 <h6 className="mb-0 font-12">GST (1%)</h6>
-                <span className="font-12 font-w600 text-dark">₹1.76</span>
+                <span className="font-12 font-w600 text-dark">₹1.48</span>
               </li>
               <li>
-                <h6 className="mb-0 font-12 text-success">Special Discount</h6>
-                <span className="font-12 font-w600 text-success">-₹10.50</span>
-              </li>
-              <li>
-                <h6 className="mb-0 font-12">Additional Charges</h6>
-                <span className="font-12 font-w600 text-dark">₹5.00</span>
+                <h6 className="mb-0 font-12">Discount (4%)</h6>
+                <span className="font-12 font-w600 text-success">-₹3.00</span>
               </li>
               <li>
                 <h6 className="mb-0 font-14 text-primary">FINAL AMOUNT</h6>
-                <span className="font-14 font-w600 text-primary">₹198.00</span>
+                <span className="font-14 font-w600 text-primary">₹149.95</span>
               </li>
             </ul>
 
             <div className="deliver-location mb-4">
               <div className="d-flex align-items-center mb-3">
                 <span className="font-w600 flex-1">Deliver to</span>
-                <span className="font-w800 text-success">PARCEL</span>
+                <span className="font-w800">DINE-IN</span>
               </div>
               <h6 className="address font-14">
-                satara, maharashtra
+                Table: 219, 225 (Family)
               </h6>
             </div>
 
@@ -73,11 +69,11 @@ function OrderDetail() {
                 </li>
                 <li>
                   <span>Payment Method</span>
-                  <span className="text-dark">CASH</span>
+                  <span className="text-dark">Not Paid</span>
                 </li>
                 <li>
                   <span>Delivery On</span>
-                  <span className="text-dark">31 May 2025 02:18 PM</span>
+                  <span className="text-dark">30 May 2025 12:19 PM</span>
                 </li>
               </ul>
             </div>
@@ -88,23 +84,23 @@ function OrderDetail() {
             <ul className="dz-timeline style-2 mb-5">
               <li className="timeline-item active">
                 <h6 className="timeline-tilte">Order Created</h6>
-                <p className="timeline-date">31 May 2025 02:18 PM</p>
+                <p className="timeline-date">30 May 2025 12:19 PM</p>
               </li>
               <li className="timeline-item active">
                 <h6 className="timeline-tilte">Order Received</h6>
-                <p className="timeline-date">31 May 2025 02:18 PM</p>
+                <p className="timeline-date">30 May 2025 12:19 PM</p>
               </li>
               <li className="timeline-item active">
-                <h6 className="timeline-tilte">Order Confirmed</h6>
-                <p className="timeline-date">31 May 2025 02:18 PM</p>
+                <h6 className="timeline-tilte">Order Cooking</h6>
+                <p className="timeline-date">30 May 2025 12:19 PM</p>
               </li>
-              <li className="timeline-item active">
+              <li className="timeline-item">
                 <h6 className="timeline-tilte">Order Processed</h6>
-                <p className="timeline-date">31 May 2025 02:18 PM</p>
+                <p className="timeline-date">30 May 2025 12:19 PM</p>
               </li>
-              <li className="timeline-item active">
+              <li className="timeline-item">
                 <h6 className="timeline-tilte">Order Delivered</h6>
-                <p className="timeline-date">31 May 2025 02:18 PM</p>
+                <p className="timeline-date">30 May 2025 12:19 PM</p>
               </li>
             </ul>
 
@@ -115,17 +111,14 @@ function OrderDetail() {
               <ul>
                 <li>
                   <div className="item-content">
-                    {/* <div className="item-media media media-65">
-                      <img src="../assets/images/avatar/1.jpg" alt="user" />
-                    </div> */}
                     <div className="item-inner">
                       <div className="item-title-row">
                         <h6 className="item-title mb-1 sub-title">
-                          <a href="#">Sugatraj Sarwade</a>
+                          <a href="#">Heenann</a>
                         </h6>
                         <span className="info">
                           <i className="fa-solid me-1 fa-phone" />
-                          7972908961
+                          6264335759
                         </span>
                       </div>
                     </div>
@@ -141,18 +134,11 @@ function OrderDetail() {
               <ul>
                 <li>
                   <div className="item-content">
-                    {/* <div className="item-media media media-65">
-                      <img src="../assets/images/store/store1.jpg" alt="store" />
-                    </div> */}
                     <div className="item-inner">
                       <div className="item-title-row">
                         <h6 className="item-title mb-1 sub-title">
                           <a href="#">Bhavani Hotel</a>
                         </h6>
-                        <span className="info">
-                          <i className="fa-solid me-1 fa-phone" />
-                          6767679760
-                        </span>
                       </div>
                     </div>
                   </div>
